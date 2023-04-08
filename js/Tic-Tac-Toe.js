f7683199fc8c2f012fcfec499141e0861e8368ee
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-04-07 20:57:04 -0400

    Update Tic-Tac-Toe.js

--- js/Tic-Tac-Toe.js
+++ js/Tic-Tac-Toe.js
@@ -31,49 +31,28 @@ function play() {
     feedback.innerHTML = 'Good luck!'
 }
 
-// Define addXO() func that runs whenever player clicks a square 
-// it adds an "X" or "O" to the square (depending on whose turn it is)
+
 function addXO() {
-    //9 get the number part of the id,
-    // replacing the existing number to to X or 
     let squareNum = this.id.slice(-1);
 
-    // 9B. Toggle whose turn it is (so that would be a boolean to flip)
-    //changing the boolean by setting it equals to not itself
+    //flip the boolean
     xTurn = !xTurn;
 
-    // 10. Deploy the X or O to the clicked square (remember: "this" IS the obj 
-    //  that called the function
-    //this represent the thing that got clicked, button, div
+
      xTurn ? (this.textContent = 'X', boardObjArr[squareNum] = 'X', feedback.textContent = '0\'s turn..' ) : 
              (this.textContent = '0', boardObjArr[squareNum] = 'O', feedback.textContent = 'X\'s turn..');
-    console.log(boardObjArr);
-    // 11. Disable the occupied square so that it cannot be clicked again
+
+    //Disabling the occupied square so that it cannot be clicked again
     this.removeEventListener('click', addXO);
 
-    //12 update the board object,
-    // console.log(this.id);
-    
-    console.log(squareNum);
-
-    // 13. Beginning w move #5 (X's 3rd move), call the checkForWinner() 
-    //func after each move and increament the moves;
     moves++;
     if(moves >= 5) {
         checkForWinner();
     }
 }
 
-// After X's third move and every turn thereafter you need to automatically run the checkForWinner() func which checks to see if X or O have 3 in a row in any of the 8 winning combos
-    // there are 8 possible winning combos:
-    /* -------
-   top row, left col, diag from top left
-   middle col, right col, diag from top right
-   middle row, bottom row */
+
 function checkForWinner() {
-    // 12. Check objecdt that is storing moves / board config and if X or O has 3 in a row, declare Game Over
-    // if() { 
-    // }
 
     //check for winner upper left to right or right to left
     if (boardObjArr[0] == boardObjArr[1] && boardObjArr[1] == boardObjArr[2]) {
@@ -81,7 +60,6 @@ function checkForWinner() {
         colorizeTicTacToe(0,1,2);
 
     }else if (boardObjArr[0] == boardObjArr[3] && boardObjArr[3] == boardObjArr[6]){
-        //check if 
         feedback.textContent = `${boardObjArr[0]} Wins`;
         colorizeTicTacToe(0,3,6);
 
@@ -102,7 +80,6 @@ function checkForWinner() {
         colorizeTicTacToe(2,5,8);
 
     }else if(boardObjArr[3] == boardObjArr[4] && boardObjArr[4] == boardObjArr[5]){
-        //checking the middle row
         feedback.textContent = `${boardObjArr[3]} Wins`; 
         colorizeTicTacToe(3,4,5);
 
@@ -121,7 +98,7 @@ function checkForWinner() {
 
 }
 
-//colorize the tic tac toe
+//colorizing the tic tac toe
 function colorizeTicTacToe(tic,tac,toe) {
     squareDivs[tic].classList.add('winner');
     squareDivs[tac].classList.add('winner');
